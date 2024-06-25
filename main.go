b5b97550d6561613c6b8160900a4a120ea8f059a
irfani91 <asisten91@gmail.com>
2024-06-25 09:33:48 +0700

    feat: add alias, struct, interface

--- main.go
+++ main.go
@@ -2,57 +2,81 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-func hello() {
-	fmt.Println("Hello")
-}
+type CentiMeters float64
 
-func bagiAngka(x, y int) int {
-	if y == 0 {
-		panic("Hasil tidak dapat dibagi")
+func (c CentiMeters) isTooLong() {
+	if c > 100 {
+		fmt.Println("Toolong \n")
+	} else {
+		fmt.Println("Just Right \n")
 	}
-	return x / y
 }
 
+type rectangle struct {
+	height float64
+	width  float64
+}
+
+type shape interface {
+	getArea() float64
+	getPerimeter() float64
+}
+
+func (c rectangle) getArea() float64 {
+	return c.width * c.height
+}
+
+func (c rectangle) getPerimeter() float64 {
+	return 2*c.width + 2*c.height
+}
+
+func measureShape(s shape) {
+	fmt.Printf("shape has an area of %f\n", s.getArea())
+	fmt.Printf("shape has an perimeter of %f\n", s.getPerimeter())
+}
+
+type circle struct {
+	radius float64
+}
+
+func (c circle) getArea() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c circle) getPerimeter() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+type square struct {
+	width float64
+}
+
+func (s square) getArea() float64 {
+	return s.width * s.width
+}
+
+func (s square) getPerimeter() float64 {
+	return 4 * s.width
+}
 func main() {
-	defer func() {
-		r := recover()
-		fmt.Printf("Panic hasil recovery %v \n", r)
-	}()
-	// for i := 0; i < 5; i++ {
-	// 	fmt.Println("Looping data \n")
-	// }
-
-	// j := 0
-	// for j < 5 {
-	// 	fmt.Printf("Looping %d\n", j)
-	// 	j++
-	// }
-
-	// i := 0
-	// for {
-	// 	i++
-	// 	if i == 2 {
-	// 		continue
-	// 	}
-	// 	fmt.Printf("Loop %d\n", i)
-
-	// 	if i == 10 {
-	// 		break
-	// 	}
-	// }
-
-	// defer hello()
-	// number := []int64{1, 2, 3, 4, 5}
-	// for i, v := range number {
-	// 	fmt.Printf("i got index %d and value:%d \n", i, v)
-	// }
-	// for i := range number {
-	// 	fmt.Printf("i got index %d and value:%d \n", i, number[i])
-	// }
-
-	hasilBagi := bagiAngka(5, 0)
-	fmt.Println(hasilBagi)
-
+	//myVar := CentiMeters(7.0)
+	// fmt.Printf("Tipe %T and value %v \n", myVar, myVar)
+	// myVar.isTooLong()
+	myRectangle := rectangle{
+		width:  20,
+		height: 30,
+	}
+	myCircle := circle{
+		radius: 5,
+	}
+	mySquare := square{
+		width: 8,
+	}
+	fmt.Printf("Tipe %T and value %+v \n", myRectangle, myRectangle)
+	measureShape(myRectangle)
+	measureShape(myCircle)
+	measureShape(mySquare)
 }
