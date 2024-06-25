5ccbbc354018e124390bf49abb783396dd83890b
irfani91 <asisten91@gmail.com>
2024-06-25 08:55:00 +0700

    feat: add panic , defer, loop, range

--- main.go
+++ main.go
@@ -1,41 +1,58 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
-func main() {
-	var formatString string = "hello world"
-	namaVarible1 := "hello world"
-	fmt.Println(namaVarible1)
-	fmt.Println(formatString)
-	boolVar := true
-	fmt.Printf("Type: %T and value %v \n", boolVar, boolVar)
-	intVar := 5
-	intVar1 := int32(6)
-	intVar2 := int64(8)
-	floatVar1 := float32(6.0)
-	floatVar2 := float64(8.9)
-	runeVar := '😊'
-	bytesVar := []byte("Hello World")
-	complexVar := -7 + 3i
-	var myInterfaceVar interface{}
-	myInterfaceVar = 5
-	errVar := errors.New("errror Detected")
-	fmt.Printf("Type: %T and value %v \n", myInterfaceVar, myInterfaceVar)
-	fmt.Printf("Type: %T and value %v \n", errVar, errVar)
-	fmt.Printf("Type: %T and value %v \n", complexVar, complexVar)
-	fmt.Printf("Type: %T and value %v \n", runeVar, runeVar)
-	fmt.Printf("Type: %T and value %v \n", bytesVar, bytesVar)
-	fmt.Printf("Type: %T and value %v \n", intVar, intVar)
-	fmt.Printf("Type: %T and value %v \n", intVar1, intVar1)
-	fmt.Printf("Type: %T and value %v \n", intVar2, intVar2)
-	fmt.Printf("Type: %T and value %v \n", floatVar1, floatVar1)
-	fmt.Printf("Type: %T and value %v \n", floatVar2, floatVar2)
+func hello() {
+	fmt.Println("Hello")
 }
 
-type MethodList interface {
-	MyFunction()
-	MyFunction2(int) int
+func bagiAngka(x, y int) int {
+	if y == 0 {
+		panic("Hasil tidak dapat dibagi")
+	}
+	return x / y
+}
+
+func main() {
+	defer func() {
+		r := recover()
+		fmt.Printf("Panic hasil recovery %v \n", r)
+	}()
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println("Looping data \n")
+	// }
+
+	// j := 0
+	// for j < 5 {
+	// 	fmt.Printf("Looping %d\n", j)
+	// 	j++
+	// }
+
+	// i := 0
+	// for {
+	// 	i++
+	// 	if i == 2 {
+	// 		continue
+	// 	}
+	// 	fmt.Printf("Loop %d\n", i)
+
+	// 	if i == 10 {
+	// 		break
+	// 	}
+	// }
+
+	// defer hello()
+	// number := []int64{1, 2, 3, 4, 5}
+	// for i, v := range number {
+	// 	fmt.Printf("i got index %d and value:%d \n", i, v)
+	// }
+	// for i := range number {
+	// 	fmt.Printf("i got index %d and value:%d \n", i, number[i])
+	// }
+
+	hasilBagi := bagiAngka(5, 0)
+	fmt.Println(hasilBagi)
+
 }
